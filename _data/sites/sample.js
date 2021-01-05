a19cd4c4cbc6a6023e3a420258a438f937c36df7
Russell Heimlich <kingkool68@gmail.com>
2021-01-04 23:01:57 -0500

    Track key sites

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,12 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
+	name: "CoderPad", // optional, falls back to object key
+	description: "Key pages to speed test",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://coderpad.io/",
+		"https://coderpad.io/register",
+		"https://coderpad.io/getting-started-interviewers",
 	]
 };
