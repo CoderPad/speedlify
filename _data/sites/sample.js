cadd0aa7209f025133656b16bec81c28b436a08c
Russell Heimlich <kingkool68@gmail.com>
2021-01-04 23:13:59 -0500

    Tweak options to make the tests work

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,8 +1,10 @@
 module.exports = {
 	name: "CoderPad", // optional, falls back to object key
 	description: "Key pages to speed test",
+	skip: false,
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
+		freshChrome: "site",
 	},
 	urls: [
 		"https://coderpad.io/",
